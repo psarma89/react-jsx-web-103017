46caa627ea026684132fc51d8cddf2f2ef70c64f
psarma89 <priyam.sarma@gmail.com>
2018-01-07 10:46:33 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 // Code goes here
+import { message } from './components/foo';
+
+class Tweet extends React.Component {
+  render() {
+    return (
+      <div className="tweet">
+        <img src="http://twitter.com/some-avatar.png" className="tweet__avatar" />
+        <div className="tweet__body">
+            <p>We are writing this tweet in JSX. Holy moly!</p>
+        </div>
+      </div>
+    );
+  }
+}
